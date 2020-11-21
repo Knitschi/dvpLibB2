6225551ba53af32c8c2078647bea9135bdaaf108
Knitschi <knitschi@freenet.de>
2020-11-21 15:38:16 +0100

    Adds some functions that use the types from dvpLibC.

--- libB2.cpp
+++ libB2.cpp
@@ -1,11 +1,44 @@
 #include <dvpLibB2/libB2.h>
+
 #include <iostream>
 
 namespace dpvLibB2
 {
-	void function()
+
+	void b2UseValueType(dvpLibC::ValueType value)
 	{
-		std::cout << "BPackage function()"
-				  << "\n";
+		std::cout << "Called dpvLibB2::b2UseValueType() with argument " << value.foo << value.bar << "\n";
 	}
+
+	dvpLibC::ValueType b2ReturnValueType()
+	{
+		return dvpLibC::ValueType{ 3, 4 };
+	}
+
+	void b2UseComplexType(dvpLibC::ComplexType* value)
+	{
+		std::cout << "Called dpvLibB2::b2UseComplexType() with argument " << value->getFoo() << "\n";
+	}
+
+	std::unique_ptr<dvpLibC::ComplexType> globalComplexType = nullptr;
+
+	dvpLibC::ComplexType* b2ReturnComplexType()
+	{
+		if (!globalComplexType)
+		{
+			globalComplexType = std::make_unique<dvpLibC::ComplexType>();
+		}
+		return globalComplexType.get();
+	}
+
+	void b2SetComplexType(std::unique_ptr<dvpLibC::ComplexType> value)
+	{
+		globalComplexType = std::move(value);
+	}
+
+	std::unique_ptr<dvpLibC::ComplexType> b2CreateComplexType()
+	{
+		return std::make_unique<dvpLibC::ComplexType>();
+	}
+
 }
--- libB2.h
+++ libB2.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <memory>
+
+#include <dvpLibC/ValueType.h>
+#include <dvpLibC/ComplexType.h>
+
 #include <dvpLibB2/dvpLibB2_export.h>
 
 namespace dpvLibB2
 {
-	DVPLIBB2_EXPORT void function();
+	DVPLIBB2_EXPORT void b2UseValueType(dvpLibC::ValueType value);
+	DVPLIBB2_EXPORT dvpLibC::ValueType b2ReturnValueType();
+
+	DVPLIBB2_EXPORT void b2UseComplexType(dvpLibC::ComplexType* value);
+	DVPLIBB2_EXPORT dvpLibC::ComplexType* b2ReturnComplexType();
+
+	DVPLIBB2_EXPORT void b2SetComplexType(std::unique_ptr<dvpLibC::ComplexType> value);
+	DVPLIBB2_EXPORT std::unique_ptr<dvpLibC::ComplexType> b2CreateComplexType();
 }
